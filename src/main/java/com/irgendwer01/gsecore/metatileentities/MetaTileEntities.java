28faa06a65498d3b5d84faadafd2cb132a1a2c9b
Niels <hermsen.niels@gmail.com>
2024-09-27 16:04:19 +0200

    changed ids

--- src/main/java/com/irgendwer01/gsecore/metatileentities/MetaTileEntities.java
+++ src/main/java/com/irgendwer01/gsecore/metatileentities/MetaTileEntities.java
@@ -18,41 +18,41 @@ public class MetaTileEntities {
     public static MetaTileEntitySieve[] SIEVES = new MetaTileEntitySieve[GTValues.V.length - 1];
 
     public static void registerMetaTileEntities() {
-        STEAM_SIEVE_BRONZE = gregtech.common.metatileentities.MetaTileEntities.registerMetaTileEntity(4000,
+        STEAM_SIEVE_BRONZE = gregtech.common.metatileentities.MetaTileEntities.registerMetaTileEntity(5000,
                 new MetaTileEntitySteamSieve(new ResourceLocation(GTValues.MODID, "sieve.steam"), false));
-        STEAM_SIEVE_STEEL = gregtech.common.metatileentities.MetaTileEntities.registerMetaTileEntity(4001,
+        STEAM_SIEVE_STEEL = gregtech.common.metatileentities.MetaTileEntities.registerMetaTileEntity(5001,
                 new MetaTileEntitySteamSieve(new ResourceLocation(GTValues.MODID, "steam_sieve_steel"), true));
 
-        SIEVES[0] = gregtech.common.metatileentities.MetaTileEntities.registerMetaTileEntity(4002,
+        SIEVES[0] = gregtech.common.metatileentities.MetaTileEntities.registerMetaTileEntity(5002,
                 new MetaTileEntitySieve(new ResourceLocation(GTValues.MODID, "sieve.lv"), 1));
-        SIEVES[1] = gregtech.common.metatileentities.MetaTileEntities.registerMetaTileEntity(4003,
+        SIEVES[1] = gregtech.common.metatileentities.MetaTileEntities.registerMetaTileEntity(5003,
                 new MetaTileEntitySieve(new ResourceLocation(GTValues.MODID, "sieve.mv"), 2));
-        SIEVES[2] = gregtech.common.metatileentities.MetaTileEntities.registerMetaTileEntity(4004,
+        SIEVES[2] = gregtech.common.metatileentities.MetaTileEntities.registerMetaTileEntity(5004,
                 new MetaTileEntitySieve(new ResourceLocation(GTValues.MODID, "sieve.hv"), 3));
-        SIEVES[3] = gregtech.common.metatileentities.MetaTileEntities.registerMetaTileEntity(4005,
+        SIEVES[3] = gregtech.common.metatileentities.MetaTileEntities.registerMetaTileEntity(5005,
                 new MetaTileEntitySieve(new ResourceLocation(GTValues.MODID, "sieve.ev"), 4));
-        SIEVES[4] = gregtech.common.metatileentities.MetaTileEntities.registerMetaTileEntity(4006,
+        SIEVES[4] = gregtech.common.metatileentities.MetaTileEntities.registerMetaTileEntity(5006,
                 new MetaTileEntitySieve(new ResourceLocation(GTValues.MODID, "sieve.iv"), 5));
 
         if (getMidTier("sieve")) {
-            SIEVES[5] = gregtech.common.metatileentities.MetaTileEntities.registerMetaTileEntity(4007,
+            SIEVES[5] = gregtech.common.metatileentities.MetaTileEntities.registerMetaTileEntity(5007,
                     new MetaTileEntitySieve(new ResourceLocation(GTValues.MODID, "sieve.luv"), 6));
-            SIEVES[6] = gregtech.common.metatileentities.MetaTileEntities.registerMetaTileEntity(4008,
+            SIEVES[6] = gregtech.common.metatileentities.MetaTileEntities.registerMetaTileEntity(5008,
                     new MetaTileEntitySieve(new ResourceLocation(GTValues.MODID, "sieve.zpm"), 7));
-            SIEVES[7] = gregtech.common.metatileentities.MetaTileEntities.registerMetaTileEntity(4009,
+            SIEVES[7] = gregtech.common.metatileentities.MetaTileEntities.registerMetaTileEntity(5009,
                     new MetaTileEntitySieve(new ResourceLocation(GTValues.MODID, "sieve.uv"), 8));
         }
 
         if (getHighTier("sieve")) {
-            SIEVES[8] = gregtech.common.metatileentities.MetaTileEntities.registerMetaTileEntity(4010,
+            SIEVES[8] = gregtech.common.metatileentities.MetaTileEntities.registerMetaTileEntity(5010,
                     new MetaTileEntitySieve(new ResourceLocation(GTValues.MODID, "sieve.uhv"), 9));
-            SIEVES[9] = gregtech.common.metatileentities.MetaTileEntities.registerMetaTileEntity(4011,
+            SIEVES[9] = gregtech.common.metatileentities.MetaTileEntities.registerMetaTileEntity(5011,
                     new MetaTileEntitySieve(new ResourceLocation(GTValues.MODID, "sieve.uev"), 10));
-            SIEVES[10] = gregtech.common.metatileentities.MetaTileEntities.registerMetaTileEntity(4012,
+            SIEVES[10] = gregtech.common.metatileentities.MetaTileEntities.registerMetaTileEntity(5012,
                     new MetaTileEntitySieve(new ResourceLocation(GTValues.MODID, "sieve.uiv"), 11));
-            SIEVES[11] = gregtech.common.metatileentities.MetaTileEntities.registerMetaTileEntity(4013,
+            SIEVES[11] = gregtech.common.metatileentities.MetaTileEntities.registerMetaTileEntity(5013,
                     new MetaTileEntitySieve(new ResourceLocation(GTValues.MODID, "sieve.uxv"), 12));
-            SIEVES[12] = gregtech.common.metatileentities.MetaTileEntities.registerMetaTileEntity(4014,
+            SIEVES[12] = gregtech.common.metatileentities.MetaTileEntities.registerMetaTileEntity(5014,
                     new MetaTileEntitySieve(new ResourceLocation(GTValues.MODID, "sieve.opv"), 13));
         }
     }
